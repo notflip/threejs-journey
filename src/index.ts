ce2f9384d1560525ea5817f0b89f3bb4d9812dde
Miguel Stevens <miguel@notflip.be>
2022-04-23 09:15:09 +0200

    correct camera position

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ import * as THREE from 'three'
 // Scene
 const scene = new THREE.Scene()
 
+// Fog
+const fog = new THREE.Fog('#262837', 1, 15)
+scene.fog = fog
+
 // Cubes
 const cubeMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff })
 
@@ -14,7 +18,8 @@ for (let i = 1; i < 5; i++) {
     const distance = .6
 
     for (let j = 0; j < 4; j++) {
-        const height = Math.round(1 + Math.random() * 2)
+        // between 1 and 3, divided by 2 for scaling
+        const height = (Math.round(1 + Math.random() * 2)) / 2
         const margin = j === 0 ? 0 : .2
         const y = prevHeight + (height / 2) + margin
 
@@ -38,20 +43,25 @@ floor.rotation.x = -Math.PI * .5
 scene.add(floor)
 
 // Lights
-const aLight = new THREE.AmbientLight(0xffffff, .5)
-scene.add(aLight)
+const dirLight = new THREE.DirectionalLight(0xffffff, 1)
+const dirLightHelper = new THREE.DirectionalLightHelper(dirLight)
+dirLight.position.set(-2, 10, 1)
+scene.add(dirLight, dirLightHelper)
 
 // Camera
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 100)
-camera.position.y = 1
-camera.position.x = 0
-camera.position.z = 11
+camera.rotation.y = -Math.PI * .25
+camera.position.x = -6
+camera.position.y = 2.5
+camera.position.z = 7
 
 // Render
 const canvas = document.querySelector('canvas.webgl') as HTMLElement
 const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true })
 renderer.setSize(window.innerWidth, window.innerHeight)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.setClearColor('#262837')
+
 window.addEventListener('resize', () => {
     camera.aspect = window.innerWidth / window.innerHeight
     camera.updateProjectionMatrix()
@@ -63,10 +73,10 @@ window.addEventListener('resize', () => {
 // Controls
 const axesHelper = new THREE.AxesHelper(10)
 scene.add(axesHelper)
-const controls = new OrbitControls(camera, renderer.domElement)
+// const controls = new OrbitControls(camera, renderer.domElement)
 
 const tick = () => {
-    controls.update();
+    // controls.update();
     renderer.render(scene, camera);
     window.requestAnimationFrame(tick);
 }
